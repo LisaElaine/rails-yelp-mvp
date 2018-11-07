d209c51a7cab9ae85cddea46d5616c40a1522582
Lisa Conti <leconti2@gmail.com>
2018-11-07 12:07:10 +1100

    just a quick update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'cleaning up datatase...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address,
+    phone: Faker::PhoneNumber,
+    category: rand(%w[chinese italian japanese french belgian])
+  )
+end
+
+puts 'Finished!'
